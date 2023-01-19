1c1af83f52f4e9a470252b51f4ca38d1eb309be6
cristian-ungureanu <cristian@themeisle.com>
2023-01-19 15:03:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ class Bootstrap {
         if ( ! empty( $test_name ) ) {
             $theme_mods = get_option( $test_name . '_theme_mods' );
             if ( ! empty( $theme_mods ) ) {
-                $bool_theme_mods = [ 'neve_advanced_layout_options', 'neve_blog_list_alternative_layout', 'neve_enable_card_style', 'neve_blog_separator', 'neve_global_header', 'neve_enable_payment_icons', 'neve_enable_product_breadcrumbs', 'neve_enable_product_navigation', 'neve_enable_cart_upsells' ];
+                $bool_theme_mods = [ 'neve_advanced_layout_options', 'neve_blog_list_alternative_layout', 'neve_enable_card_style', 'neve_blog_separator', 'neve_global_header', 'neve_enable_payment_icons', 'neve_enable_product_breadcrumbs', 'neve_enable_product_navigation', 'neve_enable_cart_upsells', 'neve_checkout_boxed_layout' ];
                 foreach ( $theme_mods as $key => $value ){
                     add_filter( 'theme_mod_'.$key, function ( $value ) use ( $key, $test_name, $bool_theme_mods ) {
                         if ( 'not-exists' === get_option( $test_name . '_' . $key, 'not-exists' ) ) {
