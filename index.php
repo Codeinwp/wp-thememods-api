027e6700d9a425bea8590a97bc38373c6a78ddb4
cristian-ungureanu <cristian@themeisle.com>
2023-04-21 11:16:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,15 @@ class Bootstrap {
 	public function init() {
 
 		add_action( 'rest_api_init', [ $this, 'register_route' ] );
+		
+		/*
+		 * Automagically authorize every request
+		 * INSECURE! DANGER! ONLY USE IN LOCAL ENVIRONMENT.
+		 */
+		add_filter( 'rest_authentication_errors', function(){
+		    wp_set_current_user( 1 ); // replace with the ID of a WP user with the authorization you want
+		}, 101 );
+		
 		$this->update_empty_option();
 		$this->filter_theme_mods();
 	}
